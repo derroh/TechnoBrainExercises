50e638568e6d1d4ebda4cc988bb0d2c2f375bdb8
Derrick <derrickwitness@gmail.com>
2021-07-31 09:59:57 +0300

    My submission

---
+++ Exercise03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercise03
+{
+    using Exercise02; //using our class library
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = null;
+            Console.WriteLine("Enter a number: ");
+            number = Console.ReadLine();
+            
+            //get rid of commas before passing to our class library
+            number = number.Replace(",", "");
+            //output message
+            Console.Write(Exercise02.Towards(number));
+
+            Console.ReadKey();
+        }
+    }
+}
